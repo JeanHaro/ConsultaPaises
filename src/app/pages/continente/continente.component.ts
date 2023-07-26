162c55140df6680f97c02540acc20bc0cedc3a8d
Jean Haro <JeanCHL98@hotmail.com>
2023-07-26 02:48:46 -0500

    Busqueda de pais por continente hecho

--- src/app/pages/continente/continente.component.ts
+++ src/app/pages/continente/continente.component.ts
@@ -1,10 +1,49 @@
 import { Component } from '@angular/core';
 
+// Modelos
+import { Pais } from 'src/app/models/pais.model';
+
+// Servicios
+import { BusquedasService } from 'src/app/services/busquedas.service';
+
 @Component({
   selector: 'app-continente',
   templateUrl: './continente.component.html',
   styleUrls: ['./continente.component.scss']
 })
 export class ContinenteComponent {
+  public pais: Pais[] = [];
+  public paisTemp: Pais[] = [];
+  public errores: boolean = false;
 
+  constructor ( private busquedaService: BusquedasService ) { }
+
+  // TODO: Buscar
+  buscar (termino: string = '') {
+    // Si está vacio entonces no regrese nada
+    if (termino.length === 0) {
+      this.errores = false;
+
+      return this.pais =  this.paisTemp;
+    }
+
+    this.busquedaService.buscar('region', termino)
+    .subscribe({
+      next: (resp: any[]) => {
+        this.pais = resp;
+        this.errores = false;
+
+        // Guardamos valores en order
+        for (let i = 0; i < this.pais.length; i++) {
+          this.pais[i].order = i + 1;
+        }
+      },
+      error: () => {
+        this.pais =  this.paisTemp;
+        this.errores = true;
+      }
+    });
+
+    return [];
+  }
 }
