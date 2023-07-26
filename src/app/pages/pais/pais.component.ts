4df9165cb27e2f4357916beae7f4500822f963ce
Jean Haro <JeanCHL98@hotmail.com>
2023-07-26 10:45:28 -0500

    Reduciendo codigo repetido2

--- src/app/pages/pais/pais.component.ts
+++ src/app/pages/pais/pais.component.ts
@@ -1,49 +1,13 @@
 import { Component } from '@angular/core';
 
-// Modelos
-import { Pais } from 'src/app/models/pais.model';
-
-// Servicios
-import { BusquedasService } from 'src/app/services/busquedas.service';
-
 @Component({
   selector: 'app-pais',
   templateUrl: './pais.component.html',
   styleUrls: ['./pais.component.scss']
 })
 export class PaisComponent {
-  public pais: Pais[] = [];
-  public paisTemp: Pais[] = [];
-  public errores: boolean = false;
-
-  constructor ( private busquedaService: BusquedasService ) { }
-
-  // TODO: Buscar
-  buscar (termino: string = '') {
-    // Si está vacio entonces no regrese nada
-    if (termino.length === 0) {
-      this.errores = false;
-
-      return this.pais =  this.paisTemp;
-    }
-
-    this.busquedaService.buscar('name', termino)
-    .subscribe({
-      next: (resp: any[]) => {
-        this.pais = resp;
-        this.errores = false;
-
-        // Guardamos valores en order
-        for (let i = 0; i < this.pais.length; i++) {
-          this.pais[i].order = i + 1;
-        }
-      },
-      error: () => {
-        this.pais =  this.paisTemp;
-        this.errores = true;
-      }
-    });
-
-    return [];
-  }
+  public tipo: string = 'name';
+  public title: string = `sus nombres`;
+  public place: string = this.title;
+  public errorText: string = '';
 }
